4cb32f84b9d5d116e2dce04bdf702a4dbae39798
Jack Temple <259976593+jack-jack-jack-jack-jack@users.noreply.github.com>
2026-04-11 11:32:49 +0100

    completed version

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // UI
+const display = document.querySelector(".display")
+
 const btns = document.querySelectorAll("button")
 btns.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -7,19 +9,32 @@ btns.forEach(btn => {
     })}
 )
 
-const display = document.querySelector(".display")
-
 // LOGIC
-const playGame = (humanChoice) => {
-    let humanScore = 0
-    let computerScore = 0 
+let humanScore = 0
+let computerScore = 0 
+let gameOver = false
 
-    const getComputerChoice = () => {
+const checkScore = () => {
+    if (humanScore === 5 || computerScore === 5) {
+        gameOver = true
+        if (humanScore > computerScore) {
+            display.textContent = "Human wins the game!"
+        } else {
+            display.textContent = "Computer wins the game!"
+        }
+    }
+}
+
+const playGame = (humanChoice) => {
+
+    const computerChoice = () => {
         const choices = ['rock', 'paper', 'scissors']
         return choices[Math.floor(Math.random() * choices.length)]
     }
 
     const playRound = (humanChoice, computerChoice) => {
+        if (gameOver) return 
+
         if (humanChoice === computerChoice) {
             display.textContent = "It's a tie!"
         } else if (
@@ -31,17 +46,13 @@ const playGame = (humanChoice) => {
         } else {
             computerScore++
         }
+
+        checkScore()
+
+        if (!gameOver) {
+            display.textContent = `The computer has a score of ${computerScore} and the human has a score of ${humanScore}`
+        }
     }
 
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-
-    if (humanScore > computerScore) {
-        display.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}.  Human wins the game!`
-    } else if (computerScore > humanScore) {
-        display.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}.  Computer wins the game!`
-    } else {
-        display.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}.  It's a tie game!`
-    }
+    playRound(humanChoice, computerChoice());
 }
-
